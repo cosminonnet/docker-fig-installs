843b5ecb3525b53507d10f7f782f9e26b7d0d263
cmolea <cmolea@unitedmotionideas.com>
2014-10-21 11:44:41 +0200

    Add SMTP support for GitLab.

--- gitlab/docker.sh
+++ gitlab/docker.sh
@@ -9,7 +9,7 @@
 
 # 1. Install Postgre Container
 # ----------------------------
-docker run --name=postgresql -d -e 'DB_NAME=gitlabhq_production' -e 'DB_USER=gitlab' -e 'DB_PASS=password' -v /var/docker-data/postgresql:/var/lib/postgresql sameersbn/postgresql:latest
+docker run --name=postgresql -d -e 'DB_NAME=gitlab_db' -e 'DB_USER=gitlab' -e 'DB_PASS=password' -v /var/docker-data/postgresql:/var/lib/postgresql sameersbn/postgresql:latest
 
 
 # 2. Install Redis Container
@@ -19,4 +19,4 @@ docker run --name=redis -d -v /var/docker-data/redis:/var/lib/redis sameersbn/re
 
 # 3. Install Gitlab Container /w Data Volume & SSL
 # ------------------------------------------------
-docker run --name='gitlab' -d -p 10022:22 -e 'GITLAB_SSH_PORT=10022' -e 'SSL_SELF_SIGNED=true' -e 'VIRTUAL_HOST=gitlab.server.com' -e 'VIRTUAL_PORT=80' -v /var/run/docker.sock:/run/docker.sock -v $(which docker):/bin/docker -v /var/docker-data/gitlab:/home/git/data --link postgresql:postgresql --link redis:redisio sameersbn/gitlab:7.3.2-1
+docker run --name='gitlab' -d -p 10022:22 -e 'GITLAB_SSH_PORT=10022' -e 'SSL_SELF_SIGNED=true' -e 'VIRTUAL_HOST=gitlab.server.com' -e 'VIRTUAL_PORT=80' -e 'SMTP_USER=user@gmail.com' -e 'SMTP_PASS=password' -v /var/run/docker.sock:/run/docker.sock -v $(which docker):/bin/docker -v /var/docker-data/gitlab:/home/git/data --link postgresql:postgresql --link redis:redisio sameersbn/gitlab:7.3.2-1
